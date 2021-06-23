d060b4b49c58f40b781ddb4161e7a4e7c9e711bf
Stephen Ray Guedea <sguedea@appddictionstudio.com>
2021-06-23 16:07:39 -0500

    updated service method to save photo / displays photo after taken

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
+import { UserPhoto } from './../models/UserPhoto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
+  // properties
+  public photos: UserPhoto[] = [];
 
   constructor() { }
 
@@ -16,6 +19,12 @@ export class PhotoService {
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
+    });
+
+    // adds new captured photos to the beginning of the Photos array
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturePhoto.webPath
     })
   }
 }
